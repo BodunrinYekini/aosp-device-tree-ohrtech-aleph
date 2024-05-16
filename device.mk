9064177574c223c29ccf7b9251e1828a3d76c37b
Bodunrin Yekini <yekinibodunrin@gmail.com>
2024-05-16 16:31:59 +0100

    base fix selinux

--- device.mk
+++ device.mk
@@ -31,8 +31,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 #test
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 PRODUCT_PACKAGES += \
