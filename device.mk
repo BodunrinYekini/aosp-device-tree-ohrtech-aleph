35b13bf1a274a4278f3c4c61a6753297b578804d
Bodunrin Yekini <yekinibodunrin@gmail.com>
2024-05-17 09:49:24 +0100

    prebuilt elf

--- device.mk
+++ device.mk
@@ -6,8 +6,13 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_product.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
 
 
 
